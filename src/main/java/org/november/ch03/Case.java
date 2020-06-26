5cd3b7744dd869e14395c5df5cf8df2a60f01149
november <novemberswimmer@gmail.com>
2020-06-26 16:55:47 -0500

    Implemented listing 3.8 and 3.9

---
+++ src/main/java/org/november/ch03/Case.java
@@ -0,0 +1,32 @@
+package org.november.ch03;
+
+import java.util.function.Supplier;
+
+public class Case<T> extends Tuple<Supplier<Boolean>, Supplier<Result<T>>> {
+
+
+    public Case(Supplier<Boolean> booleanSupplier, Supplier<Result<T>> resultSupplier) {
+        super(booleanSupplier, resultSupplier);
+    }
+
+    public static <T> Case<T> mcase(Supplier<Boolean> condition, Supplier<Result<T>> value) {
+        return new Case<>(condition, value);
+    }
+    public static <T> DefaultCase<T> mcase(Supplier<Result<T>> value) {
+        return new DefaultCase<>(() -> true, value);
+    }
+
+    public static class DefaultCase<T> extends Case<T> {
+        public DefaultCase(Supplier<Boolean> booleanSupplier, Supplier<Result<T>> resultSupplier) {
+            super(booleanSupplier, resultSupplier);
+        }
+    }
+
+    @SafeVarargs
+    public static <T> Result<T> match(DefaultCase<T> defaultCase, Case<T>... matchers) {
+        for (Case<T> aCase : matchers) {
+            if (aCase._1.get()) return aCase._2.get();
+        }
+        return defaultCase._2.get();
+    }
+}
--- src/main/java/org/november/ch03/EmailValidation.java
+++ src/main/java/org/november/ch03/EmailValidation.java
@@ -3,20 +3,16 @@ package org.november.ch03;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 
+import static org.november.ch03.Case.*;
+import static org.november.ch03.Result.*;
+
 public class EmailValidation {
     static Pattern emailPattern = Pattern.compile("^[a-z0-9._@+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
 
-    static Function<String, Result<String>> emailChecker = s-> {
-        if (s == null)  {
-            return Result.failure("email must not be null");
-        } else if (s.length() == 0) {
-            return Result.failure("email must not be empty");
-        } else if (emailPattern.matcher(s).matches()) {
-            return Result.success(s);
-        } else {
-            return Result.failure("email " + s + " is invalid.");
-        }
-    };
+    static Effect<String> success = s -> System.out.println("Main sent to " + s);
+
+    static Effect<String> failure = s -> System.out.println("Error message logged: " + s);
+
 
     public static void main(String... args) {
         emailChecker.apply("this.is@my.email").bind(success, failure);
@@ -25,7 +21,11 @@ public class EmailValidation {
         emailChecker.apply("john.doe@acme.com").bind(success, failure);
     }
 
-    static Effect<String> success = s -> System.out.println("Main sent to " + s);
+    static Function<String, Result<String>> emailChecker = s-> match(
+            mcase(() -> success(s)),
+            mcase(()-> s==null, () -> failure("Email must not be null")),
+            mcase(()-> s.length() ==0, () -> failure("email must not be empty")),
+            mcase(()-> !emailPattern.matcher(s).matches(), () -> failure("email " + s + " is invalid."))
+    );
 
-    static Effect<String> failure = s -> System.out.println("Error message logged: " + s);
 }
